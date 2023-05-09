ac0254d11b59c2b7f031926ec6eb9492c779b4db
EC2 Default User <ec2-user@ip-172-31-54-48.ec2.internal>
2023-05-09 09:11:20 +0000

    SNS_7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
   before_action :authenticate_user!,only: [:create,:edit,:update,:destroy,:index]
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort_by {|x|
+        x.favorites.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     @book = Book.new
   end
 
