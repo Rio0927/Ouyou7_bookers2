b035360b1ed2563d933bd443aad30c98ac9c6ec1
EC2 Default User <ec2-user@ip-172-31-54-48.ec2.internal>
2023-09-17 08:34:52 +0000

    SNS_8a

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,8 @@ Rails.application.routes.draw do
 
   resources :book_comments, only: [:destroy]
   
-<<<<<<< HEAD
-  resources :chats, only: [:show]
-=======
   get 'chat/:id' => 'chats#show', as: 'chat'
   resources :chats, only: [:create]
->>>>>>> 99c9a39da7c049c7be5e531241cf901753018165
 
   root 'home#top'
   get 'home/about'
