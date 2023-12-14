3a95521faf37194f2c514a6734b5f8d19360d456
EC2 Default User <ec2-user@ip-172-31-54-48.ec2.internal>
2023-12-14 10:59:29 +0000

    Story_8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,15 @@ class Book < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  
   def favorited_by?(user)
     Favorite.where(user_id: user.id, book_id: self.id).exists?
   end
